987b4c48d0e35ac15d7ab79b8077951cdef2de48
cold <kholodihor@gmail.com>
2024-12-09 23:48:21 +0200

    fix: fix next-intl

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
-import { AuthCard } from "@/components/auth-card";
+import Link from "next/link";
+import { useTranslations } from "next-intl";
 
-export default async function Home() {
+export default function Home() {
+  const t = useTranslations("HomePage");
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
+        <div className="mb-20 flex flex-col items-start gap-5">
+          <div className="flex gap-5">
+            <Link href="/en">EN</Link>
+            <Link href="/ua">UA</Link>
+          </div>
+
+          <h1 className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-lg text-red-600 sm:text-left">
+            {t("title")}
+          </h1>
+        </div>
+
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -49,7 +62,6 @@ export default async function Home() {
             Read our docs
           </a>
         </div>
-        <AuthCard />
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6">
         <a
