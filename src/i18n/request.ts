fe21a36a2043ce8bd569e0b91836ed1f2df739c2
Ihor Kholod <87018852+kholodihor@users.noreply.github.com>
2024-12-09 23:25:53 +0200

    Update request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,11 +1,11 @@
 import { getRequestConfig } from "next-intl/server";
-import { routing, Locale } from "./routing"; 
+import { routing } from "./routing"; 
 
 
 export default getRequestConfig(async ({ requestLocale }) => {
   let locale = await requestLocale;
 
-  if (!locale || !routing.locales.includes(locale as Locale)) {
+  if (!locale || !routing.locales.includes(locale as any)) {
     locale = routing.defaultLocale;
   }
 
