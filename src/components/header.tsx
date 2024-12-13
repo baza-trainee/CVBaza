edbd7f1897bd83549aaac5e7092b0025cd930aee
Yevhenii <jenmikitenko@gmail.com>
2024-12-13 15:57:39 +0200

    add header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -13,19 +13,21 @@ const Header = () => {
       <div className="flex justify-between">
         <ul className="flex gap-9">
           <li>
-            <Image src={Logo} width={60} height={60} alt="logo" />
+            <Link href="/">
+              <Image src={Logo} width={60} height={60} alt="logo" />
+            </Link>
           </li>
           {headerLinks.map((l) => (
             <Link key={l.name} href={l.href}>
               <li className="flex h-full items-center justify-center">
-                <p>{l.name}</p>
+                <p className="text-body font-sans">{l.name}</p>
               </li>
             </Link>
           ))}
         </ul>
         <button className="flex items-center justify-center gap-[18px] rounded-[100px] border-2 border-blue-500 px-5 py-3">
           <Image src={Person} width={24} height={24} alt="user" />
-          <p className="text-btn text-blue-500">Miй Аккаунт</p>
+          <p className="text-btn text-blue-500 font-sans">Miй Аккаунт</p>
         </button>
       </div>
     </header>
