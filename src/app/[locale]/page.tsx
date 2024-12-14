9288b5572d9d72d93a6536076c67ecacbb52e46c
GregoryDmitriev <bjoldgv@gmail.com>
2024-12-14 15:41:36 +0200

    Add hero-section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,25 +1,13 @@
-import Link from "next/link";
 import { useTranslations } from "next-intl";
-import { UserProfile } from "@/components/user-profile";
+import HeroSection from "@/components/HeroSection/hero-section";
 
 export default function Home() {
   const t = useTranslations("HomePage");
 
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <div className="mb-20 flex flex-col items-start gap-5">
-          <div className="flex gap-5">
-            <Link href="/en">EN</Link>
-            <Link href="/ua">UA</Link>
-          </div>
-
-          <h1 className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-lg text-red-600 sm:text-left">
-            {t("title")}
-          </h1>
-
-          <UserProfile />
-        </div>
+    <div className=" font-[family-name:var(--font-geist-sans)] overflow-x-hidden">
+      <main className="row-start-2 flex flex-col items-center sm:items-start">
+        <HeroSection/>
       </main>
     </div>
   );
