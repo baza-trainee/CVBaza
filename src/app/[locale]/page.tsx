a334dee2454aa03618e6a14a8b570d385223906d
Ihor Kholod <87018852+kholodihor@users.noreply.github.com>
2024-12-15 08:42:32 +0200

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,6 @@
-import { useTranslations } from "next-intl";
 import HeroSection from "@/components/HeroSection/hero-section";
 
 export default function Home() {
-  const t = useTranslations("HomePage");
 
   return (
     <div className=" font-[family-name:var(--font-geist-sans)] overflow-x-hidden">
