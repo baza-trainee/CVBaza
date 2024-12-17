87b7f9b6b3cfcdd8925a2ef892282af5b1cd3746
Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>
2024-12-17 10:13:31 +0200

    Update page.tsx

--- src/app/[locale]/auth/register/page.tsx
+++ src/app/[locale]/auth/register/page.tsx
@@ -1,20 +1,30 @@
-import { AuthForm } from "@/components/auth/auth-form";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { AuthForm } from "@/components/auth/auth-form"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
-export default function RegisterPage({ params }: { params: { locale: "en" | "ua" } }) {
+type Locale = "en" | "ua"
+
+type PageProps = {
+  params: { locale: Locale }
+}
+
+export default function RegisterPage({ params }: PageProps) {
   const { locale } = params
+
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Card className="w-full max-w-[600px] p-[50px] ">
+      <Card className="w-full max-w-[600px] p-[50px]">
         <CardHeader className="p-0 mb-12">
           <CardTitle className="text-center text-2xl font-semibold">
             {locale === "en" ? "Create an account" : "Створіть обліковий запис"}
           </CardTitle>
         </CardHeader>
-        <CardContent className="p-0" >
+        <CardContent className="p-0">
           <AuthForm lang={locale} type="register" />
         </CardContent>
       </Card>
     </div>
-  );
+  )
+}
+
+
 }
