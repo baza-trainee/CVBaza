5c6b85990431cc22478cf079a232aad33a5345a8
Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>
2024-12-17 09:51:56 +0200

    Update page.tsx

--- src/app/[locale]/auth/register/page.tsx
+++ src/app/[locale]/auth/register/page.tsx
@@ -2,17 +2,17 @@ import { AuthForm } from "@/components/auth/auth-form";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function RegisterPage({ params }: { params: { locale: "en" | "ua" } }) {
- const {locale}=params
+  const { locale } = params
   return (
     <div className="flex min-h-screen items-center justify-center">
       <Card className="w-full max-w-[600px] p-[50px] ">
         <CardHeader className="p-0 mb-12">
           <CardTitle className="text-center text-2xl font-semibold">
-            {locale==="en"?"Create an account":"Створіть обліковий запис"}
+            {locale === "en" ? "Create an account" : "Створіть обліковий запис"}
           </CardTitle>
         </CardHeader>
         <CardContent className="p-0" >
-          <AuthForm lang={locale } type="register" />
+          <AuthForm lang={locale} type="register" />
         </CardContent>
       </Card>
     </div>
