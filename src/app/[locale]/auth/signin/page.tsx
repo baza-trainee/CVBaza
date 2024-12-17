d761e787b0c7ff395649f2f53531bfd0c5164ad5
Baza Trainee Ukraine <132754508+baza-trainee@users.noreply.github.com>
2024-12-17 09:53:03 +0200

    Update page.tsx

--- src/app/[locale]/auth/signin/page.tsx
+++ src/app/[locale]/auth/signin/page.tsx
@@ -3,10 +3,10 @@ import { AuthForm } from "@/components/auth/auth-form";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function SignInPage({ params }: { params: { locale: "en" | "ua" } }) {
-  const {locale}=params
+  const { locale } = params
   return (
     <div className="flex min-h-screen items-center justify-center">
-     <Card className="w-full max-w-[600px] p-[50px] " >
+      <Card className="w-full max-w-[600px] p-[50px] " >
         <CardHeader className="p-0 mb-12">
           <CardTitle className="text-center text-2xl font-semibold">
             {locale === "en" ? "Sign in to your account" : "Увійдіть у свій обліковий запис"}
@@ -14,7 +14,7 @@ export default function SignInPage({ params }: { params: { locale: "en" | "ua" }
         </CardHeader>
         <CardContent className="p-0">
           <AuthError />
-          <AuthForm lang={locale } type="signin" />
+          <AuthForm lang={locale} type="signin" />
         </CardContent>
       </Card>
     </div>
