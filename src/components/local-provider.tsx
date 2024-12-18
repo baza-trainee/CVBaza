8e86bac4a3cba002edd4e55f72386383688e294d
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2024-12-18 17:53:08 +0200

    feat: add a style for the 404 page for a tablet and a mobile

--- src/components/local-provider.tsx
+++ src/components/local-provider.tsx
@@ -2,18 +2,12 @@
 
 import { useEffect } from "react";
 
-// import type { Locale } from "../types/!!!local";
-
 type LocaleProviderProps = {
   children: React.ReactNode;
-  // locale: Locale;
   locale: "en" | "ua";
 };
 
-export default function LocaleProvider({
-  children,
-  locale,
-}: LocaleProviderProps) {
+export function LocaleProvider({ children, locale }: LocaleProviderProps) {
   useEffect(() => {
     document.documentElement.lang = locale;
   }, [locale]);
