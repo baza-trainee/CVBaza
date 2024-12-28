bf6bc7ff026750e89f8482b9de3f113eaf03a304
GregoryDmitriev <bjoldgv@gmail.com>
2024-12-28 12:12:44 +0200

    Fix: adaptive hero-optimization-sections

--- src/components/homepage/hero-section.tsx
+++ src/components/homepage/hero-section.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 
 export const HeroSection = () => {
   return (
-    <div className="flex w-full overflow-hidden bg-[rgb(247,249,252)] sm:pt-[60px] sm:pb-[76px] sm:flex-col sm:gap-0  md:flex-row md:gap-[25px] md:px-4 lg:h-[calc(100vh-84px)] lg:flex-row lg:gap-10 lg:px-10 md:py-20 xl:px-20 xl:py-[34px] md:justify-between 2xl:py-0 2xl:px-[120px] 2xl:flex-wrap 2xl:content-center">
-      <div className="flex w-full flex-col lg:gap-[60px] md:w-[352px] sm:px-4 md:px-0 md:gap-10 sm:gap-[60px] lg:w-[450px] 2xl:flex-wrap 2xl:content-center">
+    <div className="flex w-full overflow-hidden bg-background sm:pt-[60px] sm:pb-[76px] sm:flex-col sm:gap-0  md:flex-row md:px-4 md:h-[calc(100vh-84px)] lg:flex-row lg:gap-[39px] lg:px-10 md:py-20 xl:px-20 xl:py-[34px] md:justify-between 2xl:py-0 2xl:px-[120px] 2xl:flex-wrap 2xl:content-center  2xl:gap-[111px] xl:gap-[102px] md:items-start md:gap-[32px] xl:items-center">
+
+      <div className="flex w-full flex-col lg:gap-[60px] md:w-[352px] sm:px-4 md:px-0 md:gap-10 sm:gap-[60px] lg:w-[443px] xl:w-[450px] xl:justify-center 2xl:flex-wrap 2xl:content-center 2xl:w-[548px]">
         <div className="w-full md:flex md:flex-col lg:w-[450px]">
-          <h1 className="text-start text-3xl text-black-900 sm:text-h1-sm  md:text-h2 lg:text-left lg:text-h1 xl:pt-20 2xl:text-h1-large">
+          <h1 className="text-start text-3xl text-black-900 sm:text-h1-sm  md:text-h2 lg:text-left lg:text-h1 2xl:text-h1-large">
             Професійний конструктор резюме
           </h1>
           <p className="mx-auto w-full pt-4 text-center sm:text-body text-black-500 sm:text-base md:pt-4 md:text-start lg:mx-0 lg:w-[354px] lg:text-left 2xl:pt-6">
@@ -31,8 +32,8 @@ export const HeroSection = () => {
         </div>
       </div>
 
-      <div className="flex h-auto w-full items-start lg:mt-[34px] xl:mt-0 lg:h-[calc(100vh-84px)] lg:w-[568px] md:justify-end 2xl:flex-wrap 2xl:content-center">
-        <div className="relative max-h-[568px] w-full max-w-[568px] sm:mt-[52px] sm:h-[414px] sm:w-[414px] md:mt-8 md:h-[360px] md:w-[360px] lg:mt-[34px] 2xl:mt-0 lg:h-[460px] lg:w-[460px] xl:w-[568px] xl:h-[568px]">
+      <div className="flex items-center md:items-start lg:w-[468px] md:justify-end xl:w-[568px] 2xl:flex-wrap 2xl:justify-center 2xl:w-[637px] ">
+        <div className="relative max-h-[568px] w-full max-w-[568px] sm:mt-[52px] sm:h-[414px] sm:w-[414px] md:mt-8 md:h-[360px] md:w-[360px] lg:mt-0 2xl:mt-0 lg:h-[460px] lg:w-[460px] xl:w-[568px] xl:h-[568px] xl:items-center">
           <Image
             src="/images/hero/ellipse.png"
             alt="Ellipse"
@@ -56,21 +57,21 @@ export const HeroSection = () => {
           />
           <Image
             src="/images/hero/typography.png"
-            className="absolute sm:top-[367px] sm:right-[16px] md:right-[20px] md:top-[340px] lg:right-[-3px] lg:top-[-12px] xl:top-[43px] xl:right-[60px]"
+            className="absolute sm:top-[367px] sm:right-[16px] md:right-[20px] md:top-[340px] lg:right-[-3px] lg:top-[-2px] xl:top-[43px] xl:right-[60px]"
             alt="Typography"
             width={320}
             height={48}
           />
           <Image
             src="/images/hero/ATS.png"
-            className="absolute sm:right-[253px] sm:top-[281px] md:right-64 md:top-[250px] lg:right-[37px] lg:top-[166px] xl:right-[107px] xl:top-[216px]"
+            className="absolute sm:right-[255px] sm:top-[281px] md:right-64 md:top-[250px] lg:right-[37px] lg:top-[166px] xl:right-[107px] xl:top-[216px]"
             alt="ATS"
             width={77}
             height={28}
           />
           <Image
             src="/images/hero/AI.png"
-            className="invisible absolute lg:visible lg:bottom-[104px] lg:right-[20px] xl:bottom-[160px] xl:right-[10px]"
+            className="invisible absolute lg:visible lg:bottom-[104px] lg:right-[-16px] xl:bottom-[160px] xl:right-[10px]"
             alt="AI"
             width={181}
             height={88}
@@ -84,7 +85,7 @@ export const HeroSection = () => {
           />
           <Image
             src="/images/hero/colors.png"
-            className="absolute sm:bottom-[376px] sm:left-[83.2px] md:bottom-[355.2px] md:left-[90px] lg:bottom-[204px] lg:left-[14px] xl:bottom-[175px] xl:left-[22px]"
+            className="absolute sm:bottom-[376px] sm:left-[84px] md:bottom-[355.2px] md:left-[90px] lg:bottom-[204px] lg:left-[14px] xl:bottom-[175px] xl:left-[22px]"
             alt="Colors"
             width={250}
             height={33}
--- src/components/homepage/optimization-section.tsx
+++ src/components/homepage/optimization-section.tsx
@@ -30,7 +30,7 @@ export const OptimizationSection = () => {
   }
   return (
     <section className="flex h-auto w-full items-center overflow-hidden bg-white sm:h-[716px] md:h-[446px] lg:h-[488px] sm:py-20">
-      <div className="container flex sm:flex-col-reverse sm:px-4 md:flex-row md:gap-8 md:px-4 lg:gap-[117px] lg:px-0 lg:pl-10 xl:pl-[120px]">
+      <div className="container flex sm:flex-col-reverse sm:px-4 md:flex-row md:gap-8 md:px-4 lg:gap-[117px] lg:px-0 lg:pl-10 xl:pl-[120px] 2xl:gap-[110px]">
         <div className="h-auto w-full sm:w-[318px] sm:h-[298px] md:w-[352px] lg:w-[402px] lg:h-[328px] xl:w-[550px] xl:h-[328px]">
           <Image
             src={imageSrc}
@@ -41,8 +41,8 @@ export const OptimizationSection = () => {
           />
         </div>
 
-        <div className="flex w-full flex-col md:w-[339px] lg:w-[378px] sm:gap-4">
-          <h2 className="text-center lg:text-h2 text-blue-900 sm:text-start sm:text-h2-sm md:text-h2-md lg:pt-[50px] lg:text-left">
+        <div className="flex w-full flex-col md:w-[339px] lg:w-[378px] sm:gap-4 2xl:gap-6 2xl:w-[540px]">
+          <h2 className="text-center lg:text-h2 text-blue-900 sm:text-start sm:text-h2-sm md:text-h2-md lg:pt-[50px] lg:text-left 2xl:text-h2-2xl">
             Оптимізація резюме
           </h2>
           <p className="sm:text-body text-center text-black-500 sm:pb-[36px] sm:text-start md:pb-0 lg:text-left">
