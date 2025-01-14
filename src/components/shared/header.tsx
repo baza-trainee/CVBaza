aaf0f0c779573ea4f15668be3a841f445978fdb9
Pavlo Mitskelevych <mitskelevitchp@gmail.com>
2025-01-14 14:41:55 +0200

    fix: change Link import

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Link } from "@/i18n/routing";
 
 export const Header = () => {
   const headerLinks = [
