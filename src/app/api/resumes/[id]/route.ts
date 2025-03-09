a8d891c69c21fa704010c6221304a121edcc22eb
Kateryna <kytana2112@gmail.com>
2025-03-09 15:56:36 +0200

    feat: add resume update

--- src/app/api/resumes/[id]/route.ts
+++ src/app/api/resumes/[id]/route.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 import { db } from "@/db";
 import { educations, resumes, workExperiences } from "@/db/schema";
 import { auth } from "@/lib/auth";
+import { resumeSchema } from "../schema";
 
 const updateResumeSchema = z.object({
   title: z.string().min(1),
@@ -122,6 +123,112 @@ export async function PATCH(
     }
 
     console.error("Error updating resume:", error);
+    return NextResponse.json(
+      { message: "Failed to update resume" },
+      { status: 500 }
+    );
+  }
+}
+
+type ResumeWithRelations = typeof resumes.$inferSelect & {
+  educations: (typeof educations.$inferSelect)[];
+  workExperiences: (typeof workExperiences.$inferSelect)[];
+};
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const session = await auth();
+    if (!session?.user?.id) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    const { id } = await params;
+
+    // Get resume
+    const resume = (await db.query.resumes.findFirst({
+      where: and(eq(resumes.id, id), eq(resumes.userId, session.user.id)),
+    })) as ResumeWithRelations;
+
+    if (resume) {
+      const resumeEducations = await db.query.educations.findMany({
+        where: eq(educations.resumeId, id),
+      });
+      const resumeExpiriences = await db.query.workExperiences.findMany({
+        where: eq(workExperiences.resumeId, id),
+      });
+      resume.educations = resumeEducations;
+      resume.workExperiences = resumeExpiriences;
+    }
+
+    if (!resume) {
+      return NextResponse.json(
+        { message: "Resume not found" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(resume);
+  } catch (error) {
+    console.error("Error fetching resumes:", error);
+    return NextResponse.json(
+      { message: "Failed to fetch resumes" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  const session = await auth();
+  try {
+    if (!session?.user?.id) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const { id } = await params;
+    const body = await req.json();
+
+    const data = resumeSchema.parse(body.data);
+
+    const updatedResume = await db.transaction(async (tx) => {
+      // Delete education and work experience
+      await tx.delete(educations).where(eq(educations.resumeId, id));
+      await tx.delete(workExperiences).where(eq(workExperiences.resumeId, id));
+
+      // Update resume
+      const [resume] = await tx
+        .update(resumes)
+        .set({ ...data, updatedAt: new Date() })
+        .where(and(eq(resumes.id, id)))
+        .returning();
+
+      // Insert new education and work experience
+      if (data.educations?.length) {
+        await tx
+          .insert(educations)
+          .values(data.educations.map((edu) => ({ ...edu, resumeId: id })));
+      }
+      if (data.workExperiences?.length) {
+        await tx
+          .insert(workExperiences)
+          .values(
+            data.workExperiences.map((exp) => ({ ...exp, resumeId: id }))
+          );
+      }
+      return resume;
+    });
+    return NextResponse.json(updatedResume);
+  } catch (error) {
+    console.error("Error updating resume:", error);
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { message: "Invalid resume data", errors: error.errors },
+        { status: 400 }
+      );
+    }
     return NextResponse.json(
       { message: "Failed to update resume" },
       { status: 500 }
